06290b74d811418dfc7e7bac75050c4b6a97d31b
baswerc <corey.baswell@gmail.com>
2015-04-18 19:03:13 -0500

    Make RoutesEntry static incase servlet is instianted for each request.

--- src/main/java/org/baswell/routes/RoutesServlet.java
+++ src/main/java/org/baswell/routes/RoutesServlet.java
@@ -1,6 +1,5 @@
 package org.baswell.routes;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,22 +8,14 @@ import java.io.IOException;
 
 public class RoutesServlet extends HttpServlet
 {
-  private RoutesEntry routesEntry;
-
-  @Override
-  public void init(ServletConfig config) throws ServletException
-  {
-    super.init(config);
-    routesEntry = new RoutesEntry();
-  }
-
+  private static RoutesEntry routesEntry = new RoutesEntry();
 
   @Override
   protected void service(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException
   {
     if (!routesEntry.process(servletRequest, servletResponse))
     {
-      servletResponse.sendError(404);
+      servletResponse.setStatus(404);
     }
   }
 }
